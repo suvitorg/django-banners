ba9c74a5a4e93e53063d424d7c43d29617aa8f3c
iankin <iankin81@gmail.com>
2012-11-21 01:01:11 +0600

    add simple test

--- banners/tests.py
+++ banners/tests.py
@@ -6,11 +6,21 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+
+from banners.models import Banner, BannerItem
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class BannerTest(TestCase):
+    fixtures = ['test_data.yaml']
+
+    def setUp(self):
+        self.user = User.objects.get()
+        self.banner = Banner.objects.get()
+        self.item = BannerItem.objects.create(banner=self.banner,
+                                              content_type=ContentType.objects.get_for_model(self.user),
+                                              object_id=1)
+
+    def test_banners(self):
+        self.assertEqual(Banner.objects.get_for_target(target=self.user)[0], self.banner)
